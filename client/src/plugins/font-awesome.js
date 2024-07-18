4e126d8103ecebd7baa857274607ee479cfa4a5f
raxar <ansar.utenov@gmail.com>
2024-07-19 02:29:18 +0500

    footer navbar

--- client/src/plugins/font-awesome.js
+++
@@ -1,7 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-library.add(fas);
-
-export { FontAwesomeIcon };
--- client/src/types/font-awesome.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@/plugins/font-awesome' {
-    import { DefineComponent } from 'vue';
-    const FontAwesomeIcon: DefineComponent<{}, {}, any>;
-    export { FontAwesomeIcon };
-}
